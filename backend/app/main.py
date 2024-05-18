00698ea38f233a96ba5d2777bcc2d9afed115639
ashen-00 <61583371+ashen-00@users.noreply.github.com>
2024-05-18 01:06:03 -0500

    Add basic logging to main.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -4,7 +4,14 @@ from sqlalchemy.sql import text
 from sqlalchemy import select
 from fastapi import FastAPI
 from database import get_db
+import logging
 
+logging.basicConfig(filename='crucible.log',
+                    level=logging.INFO,
+                    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                    datefmt='%Y-%m-%d %H:%M:%S'
+                    )
+logger = logging.getLogger(__name__)
 app = FastAPI()
 
 
@@ -15,7 +22,7 @@ def read_root():
 
 @app.get("/puzzle/{id}")
 def fetch_puzzle(id: int,  db: Session = Depends(get_db)):
-
+    logger.debug(f"Information for puzzle {id} requested")
     puzzle_resultset = db.execute(text("""
     SELECT * 
     FROM   puzzle 
@@ -23,6 +30,8 @@ def fetch_puzzle(id: int,  db: Session = Depends(get_db)):
                 ON puzzle.created_by = users.id 
     WHERE  puzzle.id = :id 
     """), {'id': id}).fetchone()
+    if puzzle_resultset is None:
+        logger.debug(f"Puzzle {id} requested and no matching record was found")
 
     hint_result_set = db.execute(text("""
     SELECT Concat(clue, direction) AS clue, 
@@ -49,16 +58,20 @@ def fetch_puzzle(id: int,  db: Session = Depends(get_db)):
             "hints": hints
             }
 
+    logger.debug(f"returning response {resp}")
     return resp
 
 
 @app.post("/puzzle/{id}/{solution}")
-def validate_puzzle(id: int, solution: str, db:Session = Depends(get_db)):
+def validate_puzzle(id: int, solution: str, db: Session = Depends(get_db)):
+    logger.debug(f"Validation for puzzle {id} requested with solution {solution}")
     resultset = db.execute(text("""
     SELECT solution 
     FROM   puzzle 
     WHERE  puzzle.id = :id 
     """), {'id': id}).fetchone()
+    if resultset is None:
+        logging.debug(f"Validation for puzzle {id} requested but no record was found")
 
     actual_solution = dict(resultset)["solution"]
 
@@ -66,6 +79,7 @@ def validate_puzzle(id: int, solution: str, db:Session = Depends(get_db)):
         return "Invalid Solution (length does not match expected)"
     
     if actual_solution == solution:
+        logger.debug(f"Validation for puzzle {id} passed")
         return True
     
     indices = []
@@ -73,6 +87,7 @@ def validate_puzzle(id: int, solution: str, db:Session = Depends(get_db)):
         if actual_solution[idx] != char:
             indices.append(idx)
 
+    logger.debug(f"Validation for puzzle {id} failed at indices {indices}")
     return {"invalidIndices": indices}
 
 
