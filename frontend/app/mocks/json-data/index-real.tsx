796f3e8a94ab34e4311937421c90fe4d17db2802
Evan Von Oehsen <evanvonoehsen@Evans-MBP-2.attlocal.net>
2024-05-19 09:30:57 -0500

    added real index file

---
+++ frontend/app/mocks/json-data/index-real.tsx
@@ -0,0 +1,71 @@
+export const JSON_DATA = {
+    id: '1',
+    title: 'The birthday bash special',
+    createdOn: '2024-05-16',
+    createdBy: 'The Crucible Team',
+    boardHeight: 5,
+    boardWidth: 5,
+    initialStructure: [0,4,12,20,24],
+    hints: [
+        {
+            number: '1',
+            direction: 'down',
+            text: 'fiesta (you\'re at one of these right now!)'
+        },
+        {
+            number: '2',
+            direction: 'down',
+            text: 'running shoe brand'
+        },
+        {
+            number: '3',
+            direction: 'down',
+            text: 'There\'s 7 of them on earth'
+        },
+        {
+            number: '4',
+            direction: 'down',
+            text: 'Slide into these'
+        },
+        {
+            number: '5',
+            direction: 'down',
+            text: 'if you don\'t do this you DIE'
+        },
+        {
+            number: '9',
+            direction: 'down',
+            text: 'Abbreviation for the tiniest state'
+        },
+        {
+            number: '1',
+            direction: 'across',
+            text: 'Number 2'
+        },
+        {
+            number: '4',
+            direction: 'across',
+            text: '4 across 4 across revolution'
+        },
+        {
+            number: '6',
+            direction: 'across',
+            text: 'type of Beast'
+        },
+        {
+            number: '7',
+            direction: 'across',
+            text: 'SPORTS ITS IN THE GAME'
+        },
+        {
+            number: '8',
+            direction: 'across',
+            text: 'plan, abbreviated. maybe common in gaming'
+        },
+        {
+            number: '10',
+            direction: 'across',
+            text: 'youre the ___ to my yang :D'
+        }
+    ]
+}
